5b193aa326aee57eb4f2f9d5496559ef74cd4ffc
jakeschurch <jschurch@bryant.edu>
2017-07-22 16:47:13 -0400

    Created file the details some workings of references of variables - such as using the ref and out keyword

---
+++ C# Files/ExampleClass.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ExampleClass
+{
+    class Example
+    {
+        /// <summary>
+        ///   The main entry point for the application
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
---
+++ C# Files/Test/Test/ValRefs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValRefs
+{
+    class ValManipulator
+    {
+        public int Factorial(int num)
+        {
+            if (num == 1)
+            { return 1; }
+            else
+            { return num * Factorial(num - 1); }
+        }
+
+        public void Out2(out int num1, out int num2)
+        {
+            num1 = 100;
+            num2 = 200;
+        }
+
+        public void Swap(ref int x, ref int y)
+        {
+            int temp;
+            temp = x; x = y; y = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            ValManipulator foo = new ValManipulator();
+
+            int a;
+            int b;
+            foo.Out2(out a, out b);
+            //Console.WriteLine("a ={0}", a); Console.WriteLine("b ={0}", b);
+            //foo.Swap(ref a, ref b);
+            //Console.WriteLine("a ={0}", a); Console.WriteLine("b ={0}", b);
+            Console.ReadKey();
+        }
+    }
+}
